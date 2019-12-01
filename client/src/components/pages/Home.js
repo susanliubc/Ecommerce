8d1ff4029feb855623799e4aeacbc484475ee795
susanliubc <susanliubc@gmail.com>
2019-11-30 19:53:15 -0800

    refactor to materail css

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import data from '../../data/product.json';
-import ProductList from './productList';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {
+  addProduct,
+  deleteProduct,
+  updateQuantity
+} from '../../actions/productAction';
 
-const Home = props => {
-  return (
-    <div className='page-container'>
-      <h1>Fruit Grocery</h1>
-      <ProductList products={data.products} />
-    </div>
-  );
+class Home extends Component {
+  render() {
+    const handleClick = id => {
+      this.props.addProduct(id);
+    };
+    const productList = this.props.products.map(product => {
+      return (
+        <div className='card' key={product.id}>
+          <div className='card-image'>
+            <img src={`/products/${product.image}`} alt='loding-image' />
+          </div>
+          <div className='card-content'>
+            <span className='card-title'>{product.name}</span>
+            <p>{product.description}</p>
+            <p>
+              <b>${product.price}</b>
+            </p>
+            <div className='card-button'>
+              <button onClick={handleClick}>Add To Cart</button>
+            </div>
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div className='page-container'>
+        <h1 className='center'>Fruit Grocery</h1>
+        <div className='productlist'>{productList}</div>
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {
+  products: PropTypes.array.isRequired,
+  addProduct: PropTypes.func.isRequired,
+  deleteProduct: PropTypes.func.isRequired,
+  updateQuantity: PropTypes.func.isRequired
 };
 
-export default Home;
+const mapStateToProps = state => ({
+  products: state.product.products
+});
+
+export default connect(mapStateToProps, {
+  addProduct,
+  deleteProduct,
+  updateQuantity
+})(Home);
