91f598d301d551b43a1ce9d7c81be910d0b746c1
susanliubc <susanliubc@gmail.com>
2020-01-10 10:55:42 -0800

    add changes before deployment

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import CartList from '../CartList';
 import { connect } from 'react-redux';
--- server.js
+++ server.js
@@ -12,7 +12,16 @@ app.use(express.json());
 //Define route
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/cart', require('./routes/cart'));
+
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
